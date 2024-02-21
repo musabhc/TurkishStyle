5de8481126428223b958fbf4dee2db82fad35ba1
Musa <mbhc4235@gmail.com>
2024-02-21 14:16:24 +0300

    Base

---
+++ FunWithBalance.cs
@@ -0,0 +1,40 @@
+﻿using BepInEx;
+using UnboundLib;
+using UnboundLib.Cards;
+using FunWithBalance.Cards;
+using HarmonyLib;
+using CardChoiceSpawnUniqueCardPatch.CustomCategories;
+
+
+namespace FunWithBalance
+{
+    // These are the mods required for our mod to work
+    [BepInDependency("com.willis.rounds.unbound", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("pykess.rounds.plugins.moddingutils", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("pykess.rounds.plugins.cardchoicespawnuniquecardpatch", BepInDependency.DependencyFlags.HardDependency)]
+    // Declares our mod to Bepin
+    [BepInPlugin(ModId, ModName, Version)]
+    // The game our mod is associated with
+    [BepInProcess("Rounds.exe")]
+    public class FunWithBalance : BaseUnityPlugin
+    {
+        private const string ModId = "com.musabhc.rounds.FunWithFriends";
+        private const string ModName = "FunWithFriends";
+        public const string Version = "0.0.1"; // What version are we on (major.minor.patch)?
+        public const string ModInitials = "FwF";
+        public static SimplyCards instance { get; private set; }
+
+
+        void Awake()
+        {
+            // Use this to call any harmony patch files your mod may have
+            var harmony = new Harmony(ModId);
+            harmony.PatchAll();
+        }
+        void Start()
+        {
+            instance = this;
+            CustomCard.BuildCard<MyCardName>();
+        }
+    }
+}
